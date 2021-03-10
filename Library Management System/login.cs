ed4d865a8439d4aac55fca1e103a570f4226c060
plolon <dawid.banczak99@gmail.com>
2021-03-10 23:16:31 +0100

    Add project files.

---
+++ Library Management System/login.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Library_Management_System
+{
+    public partial class login : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-IL2LHNT;Initial Catalog=library-menagement;Integrated Security=True;Pooling=False");
+        int count = 0;
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void login_Load(object sender, EventArgs e)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+            connection.Open();
+        }
+
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT * FROM library_person WHERE username = '"+ textBox1.Text +"' AND password = '"+ textBox2.Text+"'";
+            command.ExecuteNonQuery();
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            dataAdapter.Fill(dataTable);
+            count = Convert.ToInt32(dataTable.Rows.Count.ToString());
+            if (count == 0)
+            {
+                MessageBox.Show("Username and Password doesn't match!");
+            }
+            else
+            {
+                this.Hide();
+                mdi_user mdiUser = new mdi_user();
+                mdiUser.Show();
+            }
+
+        }
+    }
+}
